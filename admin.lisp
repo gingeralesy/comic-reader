a6f658c0161deab1b8765f03f229b9250c788002
gingeralesy <gingeralesy@gmail.com>
2015-11-15 16:39:13 +0200

    Fixed the panel parameters

--- admin.lisp
+++ admin.lisp
@@ -1,7 +1,8 @@
 (in-package #:org.gingeralesy.web.comic-reader)
 
-(admin:define-panel create-comic comic-reader
-    (:access (perm comic-reader admin comic)) :tooltip "Manage your comics." :lquery (template "admin-create-comic.ctml")
+(admin:define-panel create-comic comic-reader (:access (perm admin comic)
+                                               :tooltip "Manage your comics."
+                                               :lquery (template "admin-create-comic.ctml"))
   (with-actions (error info)
       ((:save
         (setf (config-tree :comic :title) (post-var "title")
@@ -10,3 +11,9 @@
               (config-tree :comic :is-default) (post-var "is-default"))))
     (r-clip:process
      T :error error :info info)))
+
+(admin:define-panel debug-comic comic-reader (:access (perm admin comic)
+                                              :tooltip "This is a debug panel."
+                                              :lquery (template "admin-debug.ctml"))
+  (r-clip:process
+   T))
