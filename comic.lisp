9c3884279dee153cd4a49bc60c54a42d42b67542
gingeralesy <gingeralesy@gmail.com>
2015-11-28 18:16:49 +0200

    Fixed comic page fetching

--- comic.lisp
+++ comic.lisp
@@ -4,25 +4,26 @@
   "The front page of the site."
   (r-clip:process T))
 
-(define-page comic #@"comic/comic(/([a-zA-Z]+))?(/([0-9]+))?" (:uri-groups (NIL comic-id NIL page-number)
+(define-page comic #@"comic/comic(/([a-zA-Z]+))?(/([0-9]+))?" (:uri-groups (NIL comic-path NIL page-number)
                                                                :lquery (template "comic.ctml"))
   "The web page for displaying a web comic page."
-  (let* ((comic (or (comic comic-id)
+  (let* ((comic (or (comic :path comic-path)
                     (error 'request-not-found :message "Invalid comic requested.")))
-         (page (page (comic-id comic) (when page-number (parse-integer page-number :junk-allowed T)))))
+         (page (or (page (dm:field comic '_id) (when page-number (parse-integer page-number :junk-allowed T)))
+                   (error 'request-not-found :message "Comic page does not exist."))))
     (r-clip:process
      T
-     :comic-id (comic-id comic)
-     :page-number (page-number page))))
+     :comic-path (dm:field comic 'comic-path)
+     :page-number (dm:field page 'page-number))))
 
-(define-api comic/page (comic-id page-number) ()
+(define-api comic/page (comic-path page-number) ()
   "API interface for getting metadata for a comic page."
-  (let ((comic-id comic-id)
-        (page-number (when page-number (parse-integer page-number :junk-allowed T))))
-    (case (http-method *request*)
-      (:get
-       (let ((page (page comic-id page-number)))
-         (unless (and page (<= (publish-time page) (get-universal-time)))
-           (error 'request-not-found :message "Comic page does not exist."))
-         (api-output page)))
-      (T (wrong-method-error (http-method *request*))))))
+  (case (http-method *request*)
+    (:get
+     (let* ((comic (comic :path (or* comic-path)))
+            (page-number (when (or* page-number) (parse-integer page-number :junk-allowed T)))
+            (page (page (_id comic) page-number)))
+       (unless (and page (<= (publish-time page) (get-universal-time)))
+         (error 'request-not-found :message "Comic page does not exist."))
+       (api-output page)))
+    (T (wrong-method-error (http-method *request*)))))
