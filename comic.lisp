c1b7f4e893267c3d4b6db63f7c94c3953864d192
Janne <gingeralesy@gmail.com>
2015-12-04 20:23:44 +0200

    Set the image as the background for the reader div

--- comic.lisp
+++ comic.lisp
@@ -4,12 +4,15 @@
   "The front page of the site."
   (r-clip:process T))
 
-(define-page comic #@"comic/comic(/([a-zA-Z]+))?(/([0-9]+))?" (:uri-groups (NIL comic-path NIL page-number)
-                                                               :lquery (template "comic.ctml"))
+(define-page comic #@"comic/comic(/([a-zA-Z]+))?(/([0-9]+))?"
+    (:uri-groups (NIL comic-path NIL page-number)
+     :lquery (template "comic.ctml"))
   "The web page for displaying a web comic page."
   (let* ((comic (or (comic :path comic-path)
                     (error 'request-not-found :message "Invalid comic requested.")))
-         (page (or (page (dm:field comic '_id) :page-number (when (or* page-number) (parse-integer page-number :junk-allowed T)))
+         (page (or (page (dm:field comic '_id)
+                         :page-number (when (or* page-number)
+                                        (parse-integer page-number :junk-allowed T)))
                    (error 'request-not-found :message "Comic page does not exist."))))
     (r-clip:process
      T
